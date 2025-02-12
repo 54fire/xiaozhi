ec0e1daa5a7444dcbd133041129000c250f90913
ppyuesheng <1052233445@qq.com>
2025-02-12 13:31:40 +0800

    format broad.

--- main/boards/bread-ppyuesheng-wifi/bread-ppyuesheng.cc
+++ main/boards/bread-ppyuesheng-wifi/bread-ppyuesheng.cc
@@ -17,8 +17,7 @@
 LV_FONT_DECLARE(font_puhui_14_1);
 LV_FONT_DECLARE(font_awesome_14_1);
 
-class CompactWifiBoard : public WifiBoard
-{
+class CompactWifiBoard : public WifiBoard {
 private:
     i2c_master_bus_handle_t display_i2c_bus_;
     Button boot_button_;
@@ -26,8 +25,7 @@ private:
     Button volume_up_button_;
     Button volume_down_button_;
 
-    void InitializeDisplayI2c()
-    {
+    void InitializeDisplayI2c() {
         i2c_master_bus_config_t bus_config = {
             .i2c_port = (i2c_port_t)0,
             .sda_io_num = DISPLAY_SDA_PIN,
@@ -43,92 +41,89 @@ private:
         ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &display_i2c_bus_));
     }
 
-    void InitializeButtons()
-    {
-        boot_button_.OnClick([this]()
-                             {
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
             auto& app = Application::GetInstance();
             if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
                 ResetWifiConfiguration();
             }
-            app.ToggleChatState(); });
-        touch_button_.OnPressDown([this]()
-                                  { Application::GetInstance().StartListening(); });
-        touch_button_.OnPressUp([this]()
-                                { Application::GetInstance().StopListening(); });
+            app.ToggleChatState();
+        });
+        touch_button_.OnPressDown([this]() {
+            Application::GetInstance().StartListening();
+        });
+        touch_button_.OnPressUp([this]() {
+            Application::GetInstance().StopListening();
+        });
 
-        volume_up_button_.OnClick([this]()
-                                  {
+        volume_up_button_.OnClick([this]() {
             auto codec = GetAudioCodec();
             auto volume = codec->output_volume() + 10;
             if (volume > 100) {
                 volume = 100;
             }
             codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification("音量 " + std::to_string(volume)); });
+            GetDisplay()->ShowNotification("音量 " + std::to_string(volume));
+        });
 
-        volume_up_button_.OnLongPress([this]()
-                                      {
+        volume_up_button_.OnLongPress([this]() {
             GetAudioCodec()->SetOutputVolume(100);
-            GetDisplay()->ShowNotification("最大音量"); });
+            GetDisplay()->ShowNotification("最大音量");
+        });
 
-        volume_down_button_.OnClick([this]()
-                                    {
+        volume_down_button_.OnClick([this]() {
             auto codec = GetAudioCodec();
             auto volume = codec->output_volume() - 10;
             if (volume < 0) {
                 volume = 0;
             }
             codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification("音量 " + std::to_string(volume)); });
+            GetDisplay()->ShowNotification("音量 " + std::to_string(volume));
+        });
 
-        volume_down_button_.OnLongPress([this]()
-                                        {
+        volume_down_button_.OnLongPress([this]() {
             GetAudioCodec()->SetOutputVolume(0);
-            GetDisplay()->ShowNotification("已静音"); });
+            GetDisplay()->ShowNotification("已静音");
+        });
     }
 
     // 物联网初始化，添加对 AI 可见设备
-    void InitializeIot()
-    {
-        auto &thing_manager = iot::ThingManager::GetInstance();
+    void InitializeIot() {
+        auto& thing_manager = iot::ThingManager::GetInstance();
         thing_manager.AddThing(iot::CreateThing("Speaker"));
         thing_manager.AddThing(iot::CreateThing("Lamp"));
     }
 
 public:
-    CompactWifiBoard() : boot_button_(BOOT_BUTTON_GPIO),
-                         touch_button_(TOUCH_BUTTON_GPIO),
-                         volume_up_button_(VOLUME_UP_BUTTON_GPIO),
-                         volume_down_button_(VOLUME_DOWN_BUTTON_GPIO)
-    {
+    CompactWifiBoard() :
+        boot_button_(BOOT_BUTTON_GPIO),
+        touch_button_(TOUCH_BUTTON_GPIO),
+        volume_up_button_(VOLUME_UP_BUTTON_GPIO),
+        volume_down_button_(VOLUME_DOWN_BUTTON_GPIO) {
         InitializeDisplayI2c();
         InitializeButtons();
         InitializeIot();
     }
 
-    virtual Led *GetLed() override
-    {
+    virtual Led* GetLed() override {
         static SingleLed led(BUILTIN_LED_GPIO);
         return &led;
     }
 
-    virtual AudioCodec *GetAudioCodec() override
-    {
+    virtual AudioCodec* GetAudioCodec() override {
 #ifdef AUDIO_I2S_METHOD_SIMPLEX
         static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-                                               AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
 #else
         static NoAudioCodecDuplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-                                              AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
+            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
 #endif
         return &audio_codec;
     }
 
-    virtual Display *GetDisplay() override
-    {
+    virtual Display* GetDisplay() override {
         static Ssd1306Display display(display_i2c_bus_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y,
-                                      &font_puhui_14_1, &font_awesome_14_1);
+                                    &font_puhui_14_1, &font_awesome_14_1);
         return &display;
     }
 };
