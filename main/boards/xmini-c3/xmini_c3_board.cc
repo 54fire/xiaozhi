f1265dc48d65a4203b984951b703b9255c3fde46
Jesion <458079921@qq.com>
2025-05-23 09:15:39 +0800

    更新信息
    - 恢复 xmini-c3 的代码

--- main/boards/xmini-c3/xmini_c3_board.cc
+++ main/boards/xmini-c3/xmini_c3_board.cc
@@ -16,46 +16,45 @@
 #include <driver/i2c_master.h>
 #include <esp_lcd_panel_ops.h>
 #include <esp_lcd_panel_vendor.h>
-#include "offline_scene_manager.h"
 
 #define TAG "XminiC3Board"
 
-class XminiC3Board : public WifiBoard
-{
+LV_FONT_DECLARE(font_puhui_14_1);
+LV_FONT_DECLARE(font_awesome_14_1);
+
+class XminiC3Board : public WifiBoard {
 private:
     i2c_master_bus_handle_t codec_i2c_bus_;
+    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
+    esp_lcd_panel_handle_t panel_ = nullptr;
+    Display* display_ = nullptr;
     Button boot_button_;
-    Button next_button_;
-    Button prev_button_;
     bool press_to_talk_enabled_ = false;
-    PowerSaveTimer *power_save_timer_;
-    OfflineSceneManager *manager_ = nullptr;
+    PowerSaveTimer* power_save_timer_;
 
-    void InitializePowerSaveTimer()
-    {
-        power_save_timer_ = new PowerSaveTimer(160, 10);
-        power_save_timer_->OnEnterSleepMode([this]()
-                                            {
+    void InitializePowerSaveTimer() {
+        power_save_timer_ = new PowerSaveTimer(160, 60);
+        power_save_timer_->OnEnterSleepMode([this]() {
             ESP_LOGI(TAG, "Enabling sleep mode");
             auto display = GetDisplay();
             display->SetChatMessage("system", "");
             display->SetEmotion("sleepy");
             
             auto codec = GetAudioCodec();
-            codec->EnableInput(false); });
-        power_save_timer_->OnExitSleepMode([this]()
-                                           {
+            codec->EnableInput(false);
+        });
+        power_save_timer_->OnExitSleepMode([this]() {
             auto codec = GetAudioCodec();
             codec->EnableInput(true);
             
             auto display = GetDisplay();
             display->SetChatMessage("system", "");
-            display->SetEmotion("neutral"); });
+            display->SetEmotion("neutral");
+        });
         power_save_timer_->SetEnabled(true);
     }
 
-    void InitializeCodecI2c()
-    {
+    void InitializeCodecI2c() {
         // Initialize I2C peripheral
         i2c_master_bus_config_t i2c_bus_cfg = {
             .i2c_port = I2C_NUM_0,
@@ -72,157 +71,116 @@ private:
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
     }
 
-    void ToggleChatState()
-    {
-        auto &app = Application::GetInstance();
-        if (!manager_->isOnlineScene())
-        {
-            app.StopPlaybackAndReset();
+    void InitializeSsd1306Display() {
+        // SSD1306 config
+        esp_lcd_panel_io_i2c_config_t io_config = {
+            .dev_addr = 0x3C,
+            .on_color_trans_done = nullptr,
+            .user_ctx = nullptr,
+            .control_phase_bytes = 1,
+            .dc_bit_offset = 6,
+            .lcd_cmd_bits = 8,
+            .lcd_param_bits = 8,
+            .flags = {
+                .dc_low_on_data = 0,
+                .disable_control_phase = 0,
+            },
+            .scl_speed_hz = 400 * 1000,
+        };
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c_v2(codec_i2c_bus_, &io_config, &panel_io_));
+
+        ESP_LOGI(TAG, "Install SSD1306 driver");
+        esp_lcd_panel_dev_config_t panel_config = {};
+        panel_config.reset_gpio_num = -1;
+        panel_config.bits_per_pixel = 1;
+
+        esp_lcd_panel_ssd1306_config_t ssd1306_config = {
+            .height = static_cast<uint8_t>(DISPLAY_HEIGHT),
+        };
+        panel_config.vendor_config = &ssd1306_config;
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(panel_io_, &panel_config, &panel_));
+        ESP_LOGI(TAG, "SSD1306 driver installed");
+
+        // Reset the display
+        ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_));
+        if (esp_lcd_panel_init(panel_) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to initialize display");
+            display_ = new NoDisplay();
             return;
         }
-       
-        if (!WifiStation::GetInstance().IsConnected())
-        {
-            // TODO Reboot the device, not reboot wifi
-            ResetWifiConfiguration();
-            return;
-        }
-        if (!press_to_talk_enabled_)
-        {
-            app.ToggleChatState();
-        }
+
+        // Set the display to on
+        ESP_LOGI(TAG, "Turning display on");
+        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
+
+        display_ = new OledDisplay(panel_io_, panel_, DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y,
+            {&font_puhui_14_1, &font_awesome_14_1});
     }
 
-    void InitializeButtons()
-    {
+    void InitializeButtons() {
         boot_button_.OnClick([this]() {
-            ToggleChatState();
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
+                ResetWifiConfiguration();
+            }
+            if (!press_to_talk_enabled_) {
+                app.ToggleChatState();
+            }
+        });
+        boot_button_.OnPressDown([this]() {
             power_save_timer_->WakeUp();
-        });
-
-        boot_button_.OnLongPress([this]() { 
-            manager_->switchToNextScene();
-            auto &app = Application::GetInstance();
-            if (!manager_->isOnlineScene()) {
-                // wifi is not connected or connecting, end network
-                if (!WifiStation::GetInstance().IsConnected()) {
-                    EndNetwork();
-                } else {
-                    auto &app = Application::GetInstance();
-                    app.CloseProtocol();
-                }
-            } else {
-                app.InitProtocol();
-            }
-            app.SetDeviceState(kDeviceStateIdle);
-        });
-
-        // boot_button_.OnPressDown([this]()
-        //                          {
-        //     power_save_timer_->WakeUp();
-        //     if (press_to_talk_enabled_)
-        //     {
-        //         Application::GetInstance().StartListening();
-        //     } });
-        // boot_button_.OnPressUp([this]()
-        //                        {
-        //     if (press_to_talk_enabled_)
-        //     {
-        //         Application::GetInstance().StopListening();
-        //     } });
-
-        next_button_.OnClick([this]() {
-            ToggleChatState();
-            power_save_timer_->WakeUp();
-            if (!manager_->isOnlineScene()){
-                manager_->playNextSound();
+            if (press_to_talk_enabled_) {
+                Application::GetInstance().StartListening();
             }
         });
-        next_button_.OnLongPress([this]() {
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() + 10;
-            if (volume > 100)
-            {
-                volume = 100;
+        boot_button_.OnPressUp([this]() {
+            if (press_to_talk_enabled_) {
+                Application::GetInstance().StopListening();
             }
-            codec->SetOutputVolume(volume);
-            auto &app = Application::GetInstance();
-            if (volume==100) {
-                app.PlaySound(Lang::Sounds::P3_MAX_VOLUME);
-            } else {
-                app.PlaySound(Lang::Sounds::P3_SUCCESS);
-            }
-            // GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume)); 
-        });
-
-        prev_button_.OnClick([this]() {
-            ToggleChatState();
-            power_save_timer_->WakeUp();
-            if (!manager_->isOnlineScene()){
-                manager_->playPrevSound();
-            } 
-        });
-        prev_button_.OnLongPress([this]() {
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() - 10;
-            if (volume < 10)
-            {
-                volume = 10;
-            }
-            codec->SetOutputVolume(volume);
-             auto &app = Application::GetInstance();
-            if (volume==10) {
-                app.PlaySound(Lang::Sounds::P3_MIN_VOLUME);
-            } else {
-                app.PlaySound(Lang::Sounds::P3_SUCCESS);
-            }
-            // GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume)); 
         });
     }
 
     // 物联网初始化，添加对 AI 可见设备
-    void InitializeIot()
-    {
+    void InitializeIot() {
         Settings settings("vendor");
         press_to_talk_enabled_ = settings.GetInt("press_to_talk", 0) != 0;
 
-        auto &thing_manager = iot::ThingManager::GetInstance();
+        auto& thing_manager = iot::ThingManager::GetInstance();
         thing_manager.AddThing(iot::CreateThing("Speaker"));
         thing_manager.AddThing(iot::CreateThing("PressToTalk"));
     }
 
 public:
-    XminiC3Board() : boot_button_(BOOT_BUTTON_GPIO),
-                     next_button_(NEXT_BUTTON_GPIO),
-                     prev_button_(PREV_BUTTON_GPIO)
-
-    {
+    XminiC3Board() : boot_button_(BOOT_BUTTON_GPIO) {  
         // 把 ESP32C3 的 VDD SPI 引脚作为普通 GPIO 口使用
         esp_efuse_write_field_bit(ESP_EFUSE_VDD_SPI_AS_GPIO);
-        manager_ = OfflineSceneManager::getInstance();
+
         InitializeCodecI2c();
+        InitializeSsd1306Display();
         InitializeButtons();
         InitializePowerSaveTimer();
         InitializeIot();
     }
 
-    virtual Led *GetLed() override
-    {
+    virtual Led* GetLed() override {
         static SingleLed led(BUILTIN_LED_GPIO);
         return &led;
     }
 
-    virtual AudioCodec *GetAudioCodec() override
-    {
-        static Es8311AudioCodec audio_codec(codec_i2c_bus_, I2C_NUM_0, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-                                            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
-                                            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
+    virtual Display* GetDisplay() override {
+        return display_;
+    }
 
+    virtual AudioCodec* GetAudioCodec() override {
+        static Es8311AudioCodec audio_codec(codec_i2c_bus_, I2C_NUM_0, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
         return &audio_codec;
     }
 
-    void SetPressToTalkEnabled(bool enabled)
-    {
+    void SetPressToTalkEnabled(bool enabled) {
         press_to_talk_enabled_ = enabled;
 
         Settings settings("vendor", true);
@@ -230,36 +188,35 @@ public:
         ESP_LOGI(TAG, "Press to talk enabled: %d", enabled);
     }
 
-    bool IsPressToTalkEnabled()
-    {
+    bool IsPressToTalkEnabled() {
         return press_to_talk_enabled_;
     }
 };
 
 DECLARE_BOARD(XminiC3Board);
 
-namespace iot
-{
 
-    class PressToTalk : public Thing
-    {
-    public:
-        PressToTalk() : Thing("PressToTalk", "控制对话模式，一种是长按对话，一种是单击后连续对话。")
-        {
-            // 定义设备的属性
-            properties_.AddBooleanProperty("enabled", "true 表示长按说话模式，false 表示单击说话模式", []() -> bool
-                                           {
+namespace iot {
+
+class PressToTalk : public Thing {
+public:
+    PressToTalk() : Thing("PressToTalk", "控制对话模式，一种是长按对话，一种是单击后连续对话。") {
+        // 定义设备的属性
+        properties_.AddBooleanProperty("enabled", "true 表示长按说话模式，false 表示单击说话模式", []() -> bool {
             auto board = static_cast<XminiC3Board*>(&Board::GetInstance());
-            return board->IsPressToTalkEnabled(); });
+            return board->IsPressToTalkEnabled();
+        });
 
-            // 定义设备可以被远程执行的指令
-            methods_.AddMethod("SetEnabled", "启用或禁用长按说话模式，调用前需要经过用户确认", ParameterList({Parameter("enabled", "true 表示长按说话模式，false 表示单击说话模式", kValueTypeBoolean, true)}), [](const ParameterList &parameters)
-                               {
+        // 定义设备可以被远程执行的指令
+        methods_.AddMethod("SetEnabled", "启用或禁用长按说话模式，调用前需要经过用户确认", ParameterList({
+            Parameter("enabled", "true 表示长按说话模式，false 表示单击说话模式", kValueTypeBoolean, true)
+        }), [](const ParameterList& parameters) {
             bool enabled = parameters["enabled"].boolean();
             auto board = static_cast<XminiC3Board*>(&Board::GetInstance());
-            board->SetPressToTalkEnabled(enabled); });
-        }
-    };
+            board->SetPressToTalkEnabled(enabled);
+        });
+    }
+};
 
 } // namespace iot
 
